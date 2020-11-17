8e903e01127e918168e8140ebd5eccc0497823ca
Pedro Pereira <rosspool@Pedros-MBP-2.lan>
2020-11-17 12:05:12 +0000

    inital commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,54 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+  
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash_and_redirect("Article was created successfully")
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      flash_and_redirect("Article was updated successfully")
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  def flash_and_redirect(message)
+    flash[:notice] = message
+    redirect_to @article
+  end
+
+end
