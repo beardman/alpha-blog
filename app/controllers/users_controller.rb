671146b55da7f38d4f4ba9716250ab7708d7a9eb
Pedro P <pedrohpereira@gmail.com>
2020-11-18 16:19:28 +0000

    Add delete profile functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:update, :edit]
+  before_action :require_same_user, only: [:update, :edit, :destroy]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -40,6 +42,13 @@ class UsersController < ApplicationController
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles successfully delete"
+    redirect_to articles_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
@@ -47,6 +56,13 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit your own profile"
+      redirect_to @user
+    end
   end
 
 end
