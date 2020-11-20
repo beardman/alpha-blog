7254090db18b0a3f40905dc1724378cafc6f3167
Pedro P <pedrohpereira@gmail.com>
2020-11-20 10:14:15 +0000

    Add relation between article and category
    create views, routes and controller for category
    display categories in article card

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    byebug
     @article = Article.new(article_params)
     @article.user = current_user
     if @article.save
@@ -46,7 +47,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def flash_and_redirect(message)
